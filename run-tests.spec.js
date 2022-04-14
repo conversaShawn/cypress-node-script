779fe88e2f0bbbc140d052abcb6f5f5a74675756
conversaShawn <shawn.harris3510@gmail.com>
2022-04-14 16:02:20 -0700

    testing node

---
+++ run-tests.spec.js
@@ -0,0 +1,13 @@
+const cypress = require('cypress')
+
+cypress.run({
+//   reporter: 'junit',
+  // browser: 'chrome',
+//   config: {
+//     baseUrl: 'http://localhost:8080',
+//     video: true,
+//   },
+  // env: {
+  //   DEBUG: "cypress:server:util:process_profiler"
+  // },
+})
